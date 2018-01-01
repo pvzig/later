47d9eb5b1a06ed18234e7f1725be5ccaca974c44
Peter Zignego <pzignego@gmail.com>
2018-01-01 18:44:27 -0500

    Handle URLs from more places

--- Read It Later/ShareViewController.swift
+++ Read It Later/ShareViewController.swift
@@ -17,43 +17,39 @@ class ShareViewController: NSViewController {
     
     override func loadView() {
         super.loadView()
-        guard
-            let item = self.extensionContext?.inputItems[0] as? NSExtensionItem,
-            let attachments = item.attachments as? [NSItemProvider]
-        else {
+        guard let item = self.extensionContext?.inputItems[0] as? NSExtensionItem, let attachments = item.attachments as? [NSItemProvider] else {
             finish()
             return
         }
-        for provider in attachments {
-            // Safari
-            if provider.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) {
-                provider.loadItem(forTypeIdentifier: kUTTypePropertyList as String, options: nil, completionHandler: { (item, error) in
-                    let dict = item as? [String: Any]
-                    let info = dict?[NSExtensionJavaScriptPreprocessingResultsKey] as? [String: Any]
-                    let title = info?["title"] as? String
-                    if let urlString = info?["URL"] as? String, let url = URL(string: urlString) {
-                        Later.shared.saveURL(url, title: title)
-                    }
-                    Later.shared.saveGroup.notify(queue: .main) {
-                        self.finish()
-                    }
-                })
-            // Chrome
-            } else if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
-                provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { (item, error) in
-                    guard let url = item as? URL else {
-                        self.finish()
-                        return
-                    }
-                    Later.shared.saveURL(url, title: nil)
-                    Later.shared.saveGroup.notify(queue: .main) {
-                        self.finish()
-                    }
-                })
-            // Failed
-            } else {
-                self.finish()
-            }
+
+        // Safari
+        if let provider = attachments.first(where: { $0.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) }) {
+            provider.loadItem(forTypeIdentifier: kUTTypePropertyList as String, options: nil, completionHandler: { (item, error) in
+                let dict = item as? [String: Any]
+                let info = dict?[NSExtensionJavaScriptPreprocessingResultsKey] as? [String: Any]
+                let title = info?["title"] as? String
+                if let urlString = info?["URL"] as? String, let url = URL(string: urlString) {
+                    Later.shared.saveURL(url, title: title)
+                }
+                Later.shared.saveGroup.notify(queue: .main) {
+                    self.finish()
+                }
+            })
+        // Chrome
+        } else if let provider = attachments.first(where: { $0.hasItemConformingToTypeIdentifier(kUTTypeURL as String) }) {
+            provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { (item, error) in
+                guard let url = item as? URL else {
+                    self.finish()
+                    return
+                }
+                Later.shared.saveURL(url, title: nil)
+                Later.shared.saveGroup.notify(queue: .main) {
+                    self.finish()
+                }
+            })
+        // Failed
+        } else {
+            self.finish()
         }
     }
     
